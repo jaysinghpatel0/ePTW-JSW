05de4e8e2c69c115515112ea37f1f5dab605814e
jaysinghpatel0 <mrjaysingh91@gmail.com>
2025-04-20 14:56:19 +0530

    Image Capture Code in MainActivity.kt

--- app/src/main/java/com/example/eptw_jsw/MainActivity.kt
+++ app/src/main/java/com/example/eptw_jsw/MainActivity.kt
@@ -1,10 +1,19 @@
 package com.example.eptw_jsw
 
 import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 
 class MainActivity : AppCompatActivity() {
@@ -19,6 +28,23 @@ class MainActivity : AppCompatActivity() {
         webView.webViewClient = WebViewClient() // Prevents opening external browser
         webView.settings.javaScriptEnabled = true
         webView.loadUrl("http://103.38.50.194:8080/")
+    }
+
+    const val Camera_Request = 100
+    var imageUri: Uri? = null
+
+    fun openCamera() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        val photoFile = createImageFile()
+        imageUri = FileProvider.getUriForFile(this, "com.example.eptw_jsw.fileprovider", photoFile)
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
+        startActivityForResult(intent, Camera_Request)
+    }
+
+    private fun createImageFile(): File {
+        val timestamp = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
+        val storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile("JPEG_${timestamp}_", ".jpg", storageDir)
     }
 }
 
